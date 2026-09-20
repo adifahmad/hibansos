2c804ed89d42ca6df5aef6965fab6799afef4a27
Ahmad Khaironi Adifta <52273664+adifahmad@users.noreply.github.com>
2026-09-20 07:16:12 +0700

    Remove sensitive database credentials from app.js
    Removed database connection details from pool configuration.

--- app.js
+++ app.js
@@ -18,9 +18,6 @@ const { Pool } = require('pg')
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'posdb',
-  password: '12345',
-  port: 5432,
 })
 
 var indexRouter = require('./routes/index')(pool);
